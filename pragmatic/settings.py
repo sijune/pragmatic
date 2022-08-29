8133d5041021c34b0927cf0dcd5ba33d5a36cfa1
sijune <sijune0525@gmail.com>
2022-08-29 23:42:06 +0900

    Django 39 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'accountapp', # 명시를 해줘야 한다.
     'profileapp',
     'articleapp',
+    'commentapp'
 
 ]
 
